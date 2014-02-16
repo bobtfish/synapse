6857ff4dda777ba54dfcefe3d6729db3c10ad2cf
Tomas Doran <tdoran@yelp.com>
2014-02-16 11:58:58 +0000

    Make sure the cookbooks_path is fully qualified

--- .travis_solo.rb
+++ .travis_solo.rb
@@ -1,2 +1,5 @@
 Chef::Config[:file_cache_path] = '/tmp/travis-chef'
-Chef::Config[:cookbooks_path] = 'cookbooks/'
+Chef::Config[:cookbooks_path] = "#{File.dirname(__FILE__)}/cookbooks/"
+#Chef::Config[:environments_path] = "#{File.dirname(__FILE__)}/environments/"
+#Chef::Config[:environment] = 'test'
+
